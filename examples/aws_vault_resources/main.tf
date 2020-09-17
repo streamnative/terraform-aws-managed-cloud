95bee2425983ed85978f93c06d7ae0e78a093e0d
Kevin Tham <ktham@users.noreply.github.com>
2020-09-17 15:34:20 -0700

    Add GH action to run `terraform fmt` and `terraform validate`, Update gitignore

--- examples/aws_vault_resources/main.tf
+++ examples/aws_vault_resources/main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
+  region = "us-east-1"
 }
 
 module "aws_vault" {
   source = "../../provision/aws_vault_resources"
   prefix = "myorg-prod"
   resource_tags = {
-    Enviroment: "Production"
+    Enviroment : "Production"
   }
 }
